f63f77c24d73a66fd4ff7925999fa90fb8924cde
Rob Schoening <rschoening@lendingclub.com>
2014-12-07 15:04:11 -0800

    improve mock testing

--- src/test/java/io/macgyver/neorx/rest/NeoRxClientTest.java
+++ src/test/java/io/macgyver/neorx/rest/NeoRxClientTest.java
@@ -14,6 +14,9 @@
 package io.macgyver.neorx.rest;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
 import io.macgyver.neorx.rest.NeoRxClient;
 
 import org.assertj.core.api.Assertions;
@@ -21,12 +24,12 @@ import org.assertj.core.util.Lists;
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.JsonNodeType;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class NeoRxClientTest {
 
-	
 	@Test
 	public void testListParam() {
 		NeoRxClient c = new NeoRxClient();
@@ -36,32 +39,40 @@ public class NeoRxClientTest {
 		assertThat(n.get("abc").get(0).asText()).isEqualTo("x");
 		assertThat(n.get("abc").isArray()).isTrue();
 	}
+
 	@Test
 	public void testIntParam() {
 		NeoRxClient c = new NeoRxClient();
 
-	
-		Assert.assertEquals(JsonNodeType.NUMBER, c.createParameters("abc", Integer.MAX_VALUE).get("abc").getNodeType());
+		Assert.assertEquals(JsonNodeType.NUMBER,
+				c.createParameters("abc", Integer.MAX_VALUE).get("abc")
+						.getNodeType());
 	}
+
 	@Test
 	public void testLongParam() {
 		NeoRxClient c = new NeoRxClient();
 
-	
-		Assert.assertEquals(JsonNodeType.NUMBER, c.createParameters("abc", Long.MAX_VALUE).get("abc").getNodeType());
-		
+		Assert.assertEquals(JsonNodeType.NUMBER,
+				c.createParameters("abc", Long.MAX_VALUE).get("abc")
+						.getNodeType());
+
 	}
-	
+
 	@Test
 	public void testPayloadFormat() {
 		NeoRxClient c = new NeoRxClient();
 		String query = "match (n:MyNode) where n.val={a} return n";
-		ObjectNode n = c.formatPayload(query,c.createParameters("a",1));
-		
-		Assertions.assertThat(n.path("statements").get(0).path("statement").asText()).isEqualTo(query);
-		Assertions.assertThat(n.path("statements").get(0).path("parameters").size()).isEqualTo(1);
+		ObjectNode n = c.formatPayload(query, c.createParameters("a", 1));
+
+		Assertions.assertThat(
+				n.path("statements").get(0).path("statement").asText())
+				.isEqualTo(query);
+		Assertions.assertThat(
+				n.path("statements").get(0).path("parameters").size())
+				.isEqualTo(1);
 	}
-	
+
 	@Test
 	public void testDoubleParam() {
 		NeoRxClient c = new NeoRxClient();
@@ -69,6 +80,7 @@ public class NeoRxClientTest {
 		ObjectNode n = c.createParameters("abc", 1.5d);
 
 	}
+
 	@Test
 	public void testBooleanParam() {
 		NeoRxClient c = new NeoRxClient();
@@ -76,6 +88,7 @@ public class NeoRxClientTest {
 		ObjectNode n = c.createParameters("abc", true);
 
 	}
+
 	@Test
 	public void testParams() {
 		NeoRxClient c = new NeoRxClient();
@@ -90,11 +103,32 @@ public class NeoRxClientTest {
 
 	}
 
-	@Test(expected = IllegalArgumentException.class)
+	@Test
 	public void testCreateParamsWithOddArgs() {
-		NeoRxClient c = new NeoRxClient();
 
-		ObjectNode n = c.createParameters("abc", "def", "xxx");
-	
+		try {
+			NeoRxClient c = new FakeNeoRxClient();
+
+			ObjectNode n = c.createParameters("abc", "def", "xxx");
+		} catch (Exception e) {
+			
+			Assertions.assertThat(e).isExactlyInstanceOf(IllegalArgumentException.class);
+		}
+
+	}
+
+	@Test
+	public void testBasicRequest() {
+		FakeNeoRxClient f = new FakeNeoRxClient();
+
+		String response = "{\"results\":[{\"columns\":[\"p\"],\"data\":[{\"row\":[{\"name\":\"Carrie-Anne Moss\",\"born\":1967}]}]}],\"errors\":[]}";
+
+		List<JsonNode> x = FakeNeoRxClient.withResponse(response)
+				.execCypher(null).toList().toBlocking().first();
+
+		Assertions.assertThat(x).hasSize(1);
+		Assertions.assertThat(x.get(0).get("name").asText()).isEqualTo(
+				"Carrie-Anne Moss");
+
 	}
 }
