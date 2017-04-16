ddf7d94359b698f29dde001d08543cdb83fd89b4
Rob Schoening <rschoening@lendingclub.com>
2017-04-15 20:00:20 -0700

    circle

--- .ci/circle-setup.sh
+++ .ci/circle-setup.sh
@@ -10,4 +10,9 @@ curl 'https://neo4j.com/artifact.php?name=neo4j-community-3.1.3-unix.tar.gz' -o 
 tar zxvf neo4j.tgz
 cd neo4j-community*
 
+cat <<EOF >>./conf/neo4j.conf
+dbms.security.auth_enabled=false
+dbms.memory.pagecache.size=100m
+EOF
+
 ./bin/neo4j start
