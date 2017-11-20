d41c457b2854c6a39678ad4d9eb8ce85b39d873f
Rob Schoening <rschoening@lendingclub.com>
2017-11-20 10:38:27 -0800

    auto-update from revision b777e3f

---
+++ docker-build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+IMAGE_NAME=lendingclub/neo4j-build-env:latest
+
+# If we have the source available for this, go ahead and rebuild the container
+if [ -f "$THIS_DIR/../neo4j-build-env/Dockerfile" ]; then
+    $THIS_DIR/../neo4j-build-env/build.sh
+fi
+
+docker run -v $(pwd):/build -it ${IMAGE_NAME} ./gradlew clean check build
+
